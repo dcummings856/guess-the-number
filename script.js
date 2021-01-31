2675c266718c90780c2426afcdf787dd034fe74a
David <dcummings856@gmail.com>
2021-01-31 16:07:02 -0500

    added text and made function work properly

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 document.querySelector('button').addEventListener('click', returnNumber)
 
 function returnNumber() {
-  let random = Math.floor(Math.random() * 10)
+  let random = Math.floor(Math.random() * 5)
   let input = document.querySelector('input')
   console.log(random)
-  if (input.value === random) {
-    document.querySelector('h2').innerText ='YOU GUESSED THE RIGHT NUMBER!'
-  } else {
-    document.querySelector('h2').innerText = 'TRY AGAIN.'
+  console.log(input.value)
+  if (Number(input.value) === random) {
+    document.querySelector('h2').innerText =`YOU GUESSED THE RIGHT NUMBER! ${input.value}`
+    input.value = Number('')
+  } else if(Number(input.value) !== random) {
+    document.querySelector('h2').innerText = `YOU GUESSED ${input.value}, THE NUMBER WAS ${random}`
+    input.value = Number('')
   }
 }
